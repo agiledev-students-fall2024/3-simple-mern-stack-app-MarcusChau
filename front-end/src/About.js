295e52bff8c802a1096bd3511b73a3cda947eb34
Marcus Chau <mc8786@nyu.edu>
2024-09-26 04:07:14 -0400

    About me done

---
+++ front-end/src/About.js
@@ -0,0 +1,33 @@
+// front-end/src/AboutUs.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const AboutUs = () => {
+  const [aboutData, setAboutData] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_SERVER_HOSTNAME}/api/about`)
+      .then(response => {
+        setAboutData(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching About Us data:", error);
+      });
+  }, []);
+
+  if (!aboutData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>About Me</h1>
+      {aboutData.bio.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
+      <img src={`data:image/jpeg;base64,${aboutData.imageURL}`} alt={aboutData.name} />
+    </div>
+  );
+};
+
+export default AboutUs;
